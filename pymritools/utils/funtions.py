e0f0096d91d29e95e2c4cbc67af7287a00d75c4a
jochen.schmidt <jochen.schmidt@tuta.io>
2024-09-30 14:27:31 +0200

    - add gaussian window torch;

--- pymritools/utils/funtions.py
+++ pymritools/utils/funtions.py
@@ -54,3 +54,42 @@ def root_sum_of_squares(input_data: np.ndarray | torch.Tensor, dim_channel: int 
                 axis=dim_channel
             )
         )
+
+
+def gaussian_window(size: int, sigma: float, center: int = None):
+    if center is None:
+        center = int(np.round(size / 2))
+    ax = torch.arange(size).float()
+    return torch.exp(-((ax - center) ** 2) / (2 * sigma ** 2))
+
+
+
+def gaussian_2d_kernel(size_x: int, size_y: int, center_x: int=None, center_y:int=None,
+                       sigma: float | tuple[float, float] = 1.0):
+    """
+    Generates a 2D Gaussian kernel.
+
+    Parameters:
+    - size_x (int): Kernel size in the x dimension.
+    - size_y (int): Kernel size in the y dimension.
+    - center_x (float): Center of the Gaussian in the x dimension. If None, defaults to the center of the kernel.
+    - center_y (float): Center of the Gaussian in the y dimension. If None, defaults to the center of the kernel.
+    - sigma (float | tuple of floats): Standard deviation of the Gaussian.
+        If a tuple is given the function uses different gaussian scale per dim respectively.
+
+    Returns:
+    - kernel (torch.Tensor): 2D Gaussian kernel.
+    """
+    if isinstance(sigma, float | int):
+        sigma = (sigma, sigma)
+    gauss_x = gaussian_window(size_x, sigma[0], center=center_x)
+    guass_y = gaussian_window(size_y, sigma[1], center=center_y)
+
+    gauss_2d = torch.matmul(
+        gauss_x.unsqueeze(-1), guass_y.unsqueeze(0)
+    )
+    # normalize
+    gauss_2d /= gauss_2d.sum()
+
+    return gauss_2d
+
