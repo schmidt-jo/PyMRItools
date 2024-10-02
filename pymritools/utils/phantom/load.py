859a16fdffd02395900b0b81746ae115c7e3ab96
jochen.schmidt <jochen.schmidt@tuta.io>
2024-10-02 13:56:46 +0200

    - add some normalization, switch interpolation order;

--- pymritools/utils/phantom/load.py
+++ pymritools/utils/phantom/load.py
@@ -12,10 +12,16 @@ def shepp_logan_phantom(size_xy: tuple = (400, 400), as_torch_tensor: bool = Tru
     # hard code image path
     path = plib.Path(__file__).absolute().parent.joinpath("phantom").with_suffix(".png")
     im = Image.open(path).convert("L")
-    im = np.array(im)
+    im = np.array(im, dtype=np.float32)
+    max_img = np.max(im)
+    zero_mask = im == 0
     if np.sum(np.abs(np.array(size_xy) - np.array([400, 400]))) > 1e-3:
         zf = np.array(size_xy) / np.array([400, 400])
-        im = zoom(im, zf)
+        im = zoom(im, zf, order=0)
+        zero_mask = zoom(zero_mask, zf, order=0)
+    # normalize to max 1
+    im /= max_img
+    im[zero_mask] = 0
     if as_torch_tensor:
         im = torch.from_numpy(im)
     return im
