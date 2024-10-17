7c28aef66407a27bd7cb37d7c379f7dbbbd35421
Jochen Schmidt <jschmidt@cbs.mpg.de>
2024-10-17 10:46:53 +0200

    - add single channel output upon raw data read sorting;

--- pymritools/seqprog/rawdata/rd_read.py
+++ pymritools/seqprog/rawdata/rd_read.py
@@ -2,6 +2,7 @@ import logging
 import pathlib as plib
 
 import torch
+import numpy as np
 
 from pymritools.config.seqprog import RD, Sampling, PulseqParameters2D
 from pymritools.config import setup_program_logging, setup_parser
@@ -69,9 +70,9 @@ def rd_to_torch(config: RD):
     if config.visualize:
         # transform into image
         img = fft(k_space, img_to_k=False, axes=(0, 1))
-        for i in torch.randint(low=0, high=img.shape[3], size=(3,)):
-            nifti_save(data=torch.abs(img[:, :, :, i]), img_aff=aff, path_to_dir=path_out, file_name=f"naive_recon_mag_ch-{i}")
-            nifti_save(data=torch.angle(img[:, :, :, i]), img_aff=aff, path_to_dir=path_out, file_name=f"naive_recon_phase_ch-{i}")
+        for i in np.random.randint(low=0, high=img.shape[3], size=(3,)):
+            nifti_save(data=np.abs(img[:, :, :, i]), img_aff=aff, path_to_dir=path_out, file_name=f"naive_recon_mag_ch-{i}")
+            nifti_save(data=np.angle(img[:, :, :, i]), img_aff=aff, path_to_dir=path_out, file_name=f"naive_recon_phase_ch-{i}")
         # do rSoS
         img = root_sum_of_squares(img, dim_channel=-2)
         # nifti save
