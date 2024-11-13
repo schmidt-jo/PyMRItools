7f3cd7933ac16f5d75b3b19eb8a3a4664525b951
Jochen Schmidt <jschmidt@cbs.mpg.de>
2024-11-13 10:56:42 +0100

    rename base

--- pymritools/config/__init__.py
+++ pymritools/config/__init__.py
@@ -1,4 +1,4 @@
-from . import emc, rf, database, processing, modeling
 from .basic import BaseClass, setup_program_logging, setup_parser
+from . import emc, rf, database, processing, modeling
 
 __all__ = ["BaseClass", "setup_program_logging", "setup_parser", "emc", "rf", "database", "processing", "modeling"]
