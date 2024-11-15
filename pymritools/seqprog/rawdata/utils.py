4a683afc8dc63871df06a6a1d7442885a68abacb
Jochen Schmidt <jschmidt@cbs.mpg.de>
2024-11-15 19:10:19 +0100

    small renaming of function args

--- pymritools/seqprog/rawdata/utils.py
+++ pymritools/seqprog/rawdata/utils.py
@@ -8,15 +8,17 @@ from pymritools.utils import fft
 log_module = logging.getLogger(__name__)
 
 
-def remove_oversampling(data: np.ndarray | torch.Tensor, data_input_sampled_in_time: bool = True,
+def remove_oversampling(data: np.ndarray | torch.Tensor, data_in_k_space: bool = True,
                         read_dir: int = 0, os_factor: int = 2):
+    if np.abs(os_factor - 1) < 1e-9:
+        return data
     log_module.debug(f"remove oversampling")
     # get read direction dimension
     nx = data.shape[read_dir]
 
-    if data_input_sampled_in_time:
-        # data not in frequency domain, need to transform
-        data = fft(input_data=data, img_to_k=True, axes=read_dir)
+    if data_in_k_space:
+        # need to transform
+        data = fft(input_data=data, img_to_k=False, axes=read_dir)
 
     # data in freq domain, do removal
     lower_idx = int((os_factor - 1) / (2 * os_factor) * nx)
@@ -27,7 +29,7 @@ def remove_oversampling(data: np.ndarray | torch.Tensor, data_input_sampled_in_t
         move_func = torch.movedim
     data = move_func(data, read_dir, 0)[lower_idx:upper_idx]
     data = move_func(data, 0, read_dir)
-    if data_input_sampled_in_time:
-        # data was originally in time domain originally, hence we move back
-        data = fft(input_data=data, img_to_k=False, axes=read_dir)
+    if data_in_k_space:
+        # data was in k domain originally, hence we move back
+        data = fft(input_data=data, img_to_k=True, axes=read_dir)
     return data
