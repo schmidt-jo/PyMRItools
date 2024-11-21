57382f92571d887f3a4b9f5f10513c81cb2ad69d
Patrick Scheibe <patrick@halirutan.de>
2024-11-21 16:52:06 +0100

    Add todo jochen and shorter c-op implementation

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,6 +1,10 @@
 from .utils import do_performance_test
 from pymritools.recon.loraks.operators import get_idx_2d_circular_neighborhood_patches_in_shape
 
+# TODO: write test method that:
+#       1. create kspace for phantom
+#       2. create c-mapping and calculate c-matrix
+#       3. uses the adjoint c op to backproject into original k-space
 
 def test_c_k_space_pt_idxs_operator_performance():
     nx = 2048
