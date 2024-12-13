66a754634a51bbebc8d56ad2a96cc379341543bf
Jochen Schmidt <jschmidt@cbs.mpg.de>
2024-12-13 14:17:36 +0100

    fix imports

--- pymritools/utils/__init__.py
+++ pymritools/utils/__init__.py
@@ -2,11 +2,12 @@ from .funtions import root_sum_of_squares, fft, gaussian_2d_kernel, normalize_da
 from .plotting import plot_gradient_pulse
 from .data_io import nifti_save, nifti_load, numpy_save, numpy_load, torch_save, torch_load, HidePrints
 from .matrix_indexing import (
-    MatrixOperatorLowRank2D,
     get_idx_2d_rectangular_grid, get_idx_2d_grid_circle_within_radius,
     get_idx_2d_rectangular_neighborhood_patches_in_shape,
     get_idx_2d_square_neighborhood_patches_in_shape,
-    get_idx_2d_circular_neighborhood_patches_in_shape
+    get_idx_2d_circular_neighborhood_patches_in_shape,
+    get_flat_idx_circular_neighborhood_patches_in_shape,
+    get_flat_idx_square_neighborhood_patches_in_shape
 )
 from .algorithms import cgd, randomized_svd
 
@@ -16,12 +17,13 @@ __all__ = [
     "plot_gradient_pulse", "nifti_save", "nifti_load",
     "numpy_save", "numpy_load", "torch_save", "torch_load",
     "HidePrints",
-    "MatrixOperatorLowRank2D",
     "get_idx_2d_grid_circle_within_radius",
     "get_idx_2d_rectangular_grid",
     "get_idx_2d_grid_circle_within_radius",
     "get_idx_2d_rectangular_neighborhood_patches_in_shape",
     "get_idx_2d_square_neighborhood_patches_in_shape",
     "get_idx_2d_circular_neighborhood_patches_in_shape",
+    "get_flat_idx_square_neighborhood_patches_in_shape",
+    "get_flat_idx_circular_neighborhood_patches_in_shape",
     "cgd", "randomized_svd"
 ]
