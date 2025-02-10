04b851d1031f085813b58e54adab27c324e585d1
Jochen Schmidt <jschmidt@cbs.mpg.de>
2025-02-10 16:07:51 +0100

    fix minor input quirks

--- pymritools/processing/denoising/mppca/__init__.py
+++ pymritools/processing/denoising/mppca/__init__.py
@@ -4,5 +4,5 @@ import sys
 autodmri_path = plib.Path(__file__).absolute().parent.parent.joinpath("autodmri/")
 sys.path.append(autodmri_path.as_posix())
 
-from .options import Config
+from pymritools.config.processing.denoising import SettingsMPPCA
 from .denoise import denoise
