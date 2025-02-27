aebaf8dddc6b69de1b3663a706d246b47d8ae8b7
Jochen Schmidt <jschmidt@cbs.mpg.de>
2025-02-27 09:17:58 +0100

    add data utils to cmd tools

--- pymritools/seqprog/rawdata/utils.py
+++ pymritools/seqprog/rawdata/utils.py
@@ -4,9 +4,9 @@ import pathlib as plib
 
 import torch
 
-from pymritools.utils import fft, torch_load, torch_save
+from pymritools.utils import fft, torch_load, torch_save, root_sum_of_squares, nifti_save
 from pymritools.config import setup_program_logging, setup_parser
-from pymritools.config.seqprog.rawdata import RMOS
+from pymritools.config.seqprog.rawdata import RMOS, RSOS
 
 log_module = logging.getLogger(__name__)
 
@@ -38,23 +38,7 @@ def remove_oversampling(data: np.ndarray | torch.Tensor, data_in_k_space: bool =
     return data
 
 
-def rmos(settings: RMOS):
-    path_in = plib.Path(settings.input_file)
-    data = torch_load(path_in)
-    if settings.dim < 1:
-        log_module.info(f"No read direction given, deducing from shape, taking biggest dim.")
-        ds = torch.tensor(data.shape)
-        read_dir = torch.where(ds == ds.max())[0].item()
-    else:
-        read_dir = settings.dim
-    log_module.info(f"found dims: {data.shape}, set read_dir = {read_dir}")
-    data = remove_oversampling(
-        data=data, data_in_k_space=settings.data_in_kspace, read_dir=read_dir, os_factor=settings.os_factor
-    )
-    torch_save(data, path_to_file=settings.out_path, file_name=f"{path_in.stem}_rmos")
-
-
-def main():
+def rmos():
     # setup logging
     setup_program_logging(name="remove oversampling", level=logging.INFO)
     # setup parser
@@ -65,10 +49,58 @@ def main():
     # get config
     settings = RMOS.from_cli(args=args.settings, parser=parser)
     settings.display()
-
     try:
-        rmos(settings=settings)
+        path_in = plib.Path(settings.input_file)
+        data = torch_load(path_in)
+        if settings.dim < 1:
+            log_module.info(f"No read direction given, deducing from shape, taking biggest dim.")
+            ds = torch.tensor(data.shape)
+            read_dir = torch.where(ds == ds.max())[0].item()
+        else:
+            read_dir = settings.dim
+        log_module.info(f"found dims: {data.shape}, set read_dir = {read_dir}")
+        data = remove_oversampling(
+            data=data, data_in_k_space=settings.data_in_kspace, read_dir=read_dir, os_factor=settings.os_factor
+        )
+        torch_save(data, path_to_file=settings.out_path, file_name=f"{path_in.stem}_rmos")
     except Exception as e:
         parser.print_help()
         logging.exception(e)
         exit(-1)
+
+
+def recon_naive_rsos():
+    # setup logging
+    setup_program_logging(name="Naive Revon FFT Root Sum of Squares", level=logging.INFO)
+    # setup parser
+    parser, args = setup_parser(
+        prog_name="recon_fft_rsos",
+        dict_config_dataclasses={"settings": RSOS}
+    )
+    # get config
+    settings = RMOS.from_cli(args=args.settings, parser=parser)
+    settings.display()
+    try:
+        path_in = plib.Path(settings.input_file)
+        data = torch_load(path_in)
+        path_in_aff = plib.Path(settings.input_affine)
+        if path_in_aff.is_file():
+            aff = torch_load(path_in_aff)
+        else:
+            log_module.info(f"No affine provided or file not found")
+            aff = torch.eye(4)
+        if settings.data_in_kspace:
+            log_module.info(f"FFT data to image space")
+            data = fft(input_data=data, img_to_k=False, axes=settings.dim_img)
+        log_module.info(f"RSOS along channel dim specified: {settings.dim_c}")
+        data = root_sum_of_squares(
+            input_data=data, dim_channel=settings.dim_c
+        )
+
+        path_out = plib.Path(settings.out_path)
+        nifti_save(data, img_aff=aff, path_to_dir=path_out, file_name=f"{path_in.stem}_rsos")
+    except Exception as e:
+        parser.print_help()
+        logging.exception(e)
+        exit(-1)
+
