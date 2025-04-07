b96b0125df44d5a4c00e5cb6f1f788e80b67f0b4
jochen.schmidt <jochen.schmidt@tuta.io>
2025-04-07 13:38:12 +0200

    add exact data consistency variant of ac - loraks

--- pymritools/recon/loraks_dev/js_ac_loraks.py
+++ pymritools/recon/loraks_dev/js_ac_loraks.py
@@ -143,11 +143,11 @@ def recon(
     eig_vals, vecs = torch.linalg.eigh(ac_matrix.mH @ ac_matrix)
     # eigenvals and corresponding evs are in ascending order
     v = vecs[..., :-rank]
-    if use_s_matrix:
-        # complex number representation
-        v = torch.reshape(v, (2 * nce, -1, v.shape[-1]))
-        v = v[::2] + v[1::2] * 1j
-        v = torch.reshape(v, (-1, v.shape[-1]))
+    # if use_s_matrix:
+    #     # complex number representation
+    #     v = torch.reshape(v, (2 * nce, -1, v.shape[-1]))
+    #     v = v[::2] + v[1::2] * 1j
+    #     v = torch.reshape(v, (-1, v.shape[-1]))
 
     del ac_matrix
     torch.cuda.empty_cache()
@@ -187,9 +187,120 @@ def recon(
     return k_recon
 
 
+def recon_data_consistency(
+        k_space: torch.Tensor, sampling_mask: torch.Tensor,
+        rank: int, loraks_neighborhood_size: int = 5, matrix_type: str = "S",
+        max_num_iter: int = 200,
+        device: torch.device = torch.get_default_device()
+):
+    if matrix_type.capitalize() == "S":
+        use_s_matrix = True
+    elif matrix_type.capitalize() == "C":
+        use_s_matrix = False
+    else:
+        err = "Currently only S or C matrix LORAKS types are supported."
+        log_module.error(err)
+        raise ValueError(err)
+
+    ac_data, nce = find_ac_region(k_space, sampling_mask)
+    mask = sampling_mask.to(torch.bool)
+    mask_unsampled = ~mask
+
+    logging.info("Set Matrix Indices and AC Matrix")
+    # ToDo: Calculate size of AC subregion if whole region is to big for CPU?
+    ac_indices, ac_matrix_shape = get_linear_indices(
+        k_space_shape=ac_data.shape,
+        patch_shape=(loraks_neighborhood_size, loraks_neighborhood_size, -1, -1),
+        sample_directions=(1, 1, 0, 0)
+    )
+
+    if use_s_matrix:
+        ac_matrix_shape = tuple((torch.tensor(ac_matrix_shape, dtype=torch.int) * torch.tensor([2, 2])).tolist())
+        ac_matrix = s_operator(k_space=ac_data, indices=ac_indices, matrix_shape=ac_matrix_shape).to(device)
+    else:
+        ac_matrix = c_operator(k_space=ac_data, indices=ac_indices, matrix_shape=ac_matrix_shape).to(device)
+
+    eig_vals, vecs = torch.linalg.eigh(ac_matrix.mH @ ac_matrix)
+    # eigenvals and corresponding evs are in ascending order
+    v = vecs[..., :-rank]
+    # if use_s_matrix:
+    #     # complex number representation
+    #     v = torch.reshape(v, (2 * nce, -1, v.shape[-1]))
+    #     v = v[::2] + v[1::2] * 1j
+    #     v = torch.reshape(v, (-1, v.shape[-1]))
+
+    del ac_matrix
+    torch.cuda.empty_cache()
+
+    logging.info("Init optimization")
+    # we optimize only unmeasured data
+    # find read direction
+    read_dir = -1
+    for i in range(2):
+        if ac_data.shape[i] >= k_space.shape[i] - 5:
+        # tmp_mask = torch.movedim(mask[:, :, 0].to(torch.int), i, 0)
+        # if torch.sum(tmp_mask[:, tmp_mask.shape[2] // 2]) == tmp_mask.shape[0]:
+            read_dir = i
+    if read_dir < 0:
+        err = "Could not find read direction."
+        log_module.error(err)
+        raise ValueError(err)
+
+    k_init = torch.zeros_like(k_space[mask_unsampled]) * 1e-3
+    k_init = torch.movedim(k_init, read_dir, 0)
+    k_init = torch.reshape(k_init, (k_space.shape[read_dir], -1, *k_space.shape[2:]))
+    k_init = torch.movedim(k_init, 0, read_dir)
+    k = k_init.clone().contiguous().to(device).requires_grad_()
+
+    lr = torch.linspace(5e-3, 5e-4, max_num_iter, device=device)
+    mask_unsampled = mask_unsampled.to(device)
+
+    # we take the 0 filled sampled data to the device
+    k_sampled = k_space.to(device)
+    # compute the contribution to loss
+    # if use_s_matrix:
+    #     ad = fourier_mv_s(k=k_sampled.view(*k_sampled.shape[:2], -1), v=v, ps=loraks_neighborhood_size)
+    # else:
+    #     ad = fourier_mv(k=k_sampled.view(*k_sampled.shape[:2], -1), v=v, ps=loraks_neighborhood_size)
+
+    progress_bar = tqdm.trange(max_num_iter, desc="Optimization")
+    for i in progress_bar:
+        # we need a zero filling operator to compute the losses
+        tmp = torch.zeros_like(k_sampled)
+        tmp[mask_unsampled] = k.flatten()
+        if use_s_matrix:
+            mv = fourier_mv_s(k=(k_sampled + tmp).view(*tmp.shape[:2], -1), v=v, ps=loraks_neighborhood_size)
+        else:
+            mv = fourier_mv(k=(k_sampled + tmp).view(*tmp.shape[:2], -1), v=v, ps=loraks_neighborhood_size)
+
+        # we are not using the nullspace but the rank sized subspace as V,
+        # hence we want to maximize not minimize the norm
+        loss = torch.linalg.norm(mv, ord="fro")
+
+        loss.backward()
+
+        with torch.no_grad():
+            k -= lr[i] * k.grad
+
+        norm_grad = torch.linalg.norm(k.grad).item()
+        progress_bar.postfix = (
+            f"loss: {1e3 * loss.item():.2f}, norm k: {torch.linalg.norm(k).item():.3f}, norm grad: {norm_grad:.3f}"
+        )
+        k.grad.zero_()
+        if norm_grad < 1e-3:
+            msg = f"Optimization converged at step: {i+1}"
+            log_module.info(msg)
+            break
+    # build data
+    tmp = torch.zeros_like(k_sampled)
+    tmp[mask_unsampled] = k.flatten().detach()
+    k_recon = k_sampled + tmp
+    return k_recon.cpu(), tmp.cpu()
+
+
 def main_pbp():
-    logging.info("Set Device")
-    device = torch.device("cuda")
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    logging.info(f"Set Device: {device}")
 
     logging.info(f"Set Paths")
     path = plib.Path(__name__).parent.absolute()
@@ -197,33 +308,37 @@ def main_pbp():
     path_fig.mkdir(exist_ok=True, parents=True)
 
     logging.info("Set Phantom")
-    nx, ny, nc, ne = (256, 256, 3, 2)
+    nx, ny, nc, ne = (256, 256, 4, 1)
     phantom = Phantom.get_shepp_logan(shape=(nx, ny), num_coils=nc, num_echoes=ne)
-    sl_us = phantom.sub_sample_ac_weighted_lines(acceleration=3, ac_lines=30)
+    sl_us = phantom.sub_sample_ac_random_lines(acceleration=2, ac_lines=40).unsqueeze(-1)
 
     sl_us = sl_us.contiguous()
     img_us = torch.abs(fft(sl_us, axes=(0, 1)))
     # img_us = root_sum_of_squares(img_us, dim_channel=-2)
     mask = (torch.abs(sl_us) > 1e-9).to(torch.int)
 
-    k_recon = recon(
-        k_space=sl_us, sampling_mask=mask, rank=50,
-        loraks_neighborhood_size=5, lambda_reg=0.1, matrix_type="S",
-        max_num_iter=200, device=device
+    # k_recon = recon(
+    #     k_space=sl_us, sampling_mask=mask, rank=50,
+    #     loraks_neighborhood_size=5, lambda_reg=0.1, matrix_type="S",
+    #     max_num_iter=200, device=device
+    # )
+    k_recon, tmp = recon_data_consistency(
+        k_space=sl_us, sampling_mask=mask, rank=50, max_num_iter=150,
+        loraks_neighborhood_size=5, matrix_type="S", device=device
     )
     img_recon = torch.abs(fft(k_recon, axes=(0, 1)))
 
     fig = psub.make_subplots(
-        rows=2, cols=4
+        rows=3, cols=4
     )
-    for i, d in enumerate([sl_us, img_us, k_recon, img_recon]):
+    for i, d in enumerate([sl_us, img_us, k_recon, img_recon, tmp, img_recon - img_us]):
         if i % 2 == 0:
             d = torch.log(torch.abs(d))
         else:
             d = torch.abs(d)
-        row = int(i % 2) + 1
+        row = int(i / 2) + 1
         for c in range(2):
-            col = int(i / 2) + 2 * c + 1
+            col = int(i % 2) + 2 * c + 1
             fig.add_trace(
                 go.Heatmap(z=d[:, :, c, 0], showscale=False),
                 row=row, col=col
