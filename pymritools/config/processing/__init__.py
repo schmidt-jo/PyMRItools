5033a270f95c0e71abfeb660b5f15fb0d9b1c844
Jochen Schmidt <jschmidt@cbs.mpg.de>
2025-04-15 08:29:12 +0200

    move dklc

--- pymritools/config/processing/__init__.py
+++ pymritools/config/processing/__init__.py
@@ -1,6 +1,5 @@
 from .unringing import Settings as GibbsUnringingSettings
 from .denoising import SettingsMPPCA as DenoiseSettingsMPPCA
 from .denoising import SettingsMPK as DenoiseSettingsMPK
-from .denoising import SettingsKLC as DenoiseSettingsKLC
 
-__all__ = ['GibbsUnringingSettings', 'DenoiseSettingsMPPCA', 'DenoiseSettingsMPK', 'DenoiseSettingsKLC']
+__all__ = ['GibbsUnringingSettings', 'DenoiseSettingsMPPCA', 'DenoiseSettingsMPK']
