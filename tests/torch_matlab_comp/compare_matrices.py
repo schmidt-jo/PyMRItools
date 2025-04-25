a2dccb89161c7492a389a86ab842ef6e6c730fda
Jochen Schmidt <jschmidt@cbs.mpg.de>
2025-04-25 11:38:53 +0200

    start mwe for matrix operations and subspace creation - check complexify nullspace for 1 channel

--- tests/torch_matlab_comp/compare_matrices.py
+++ tests/torch_matlab_comp/compare_matrices.py
@@ -2,6 +2,9 @@ import torch
 
 
 def check_matrices(matrix_1, matrix_2, rtol=1e-04, atol=1e-07, name=""):
+    matrix_1 = torch.squeeze(matrix_1)
+    matrix_2 = torch.squeeze(matrix_2)
+
     assert matrix_1.shape == matrix_2.shape, f"{name} shape mismatch"
     # give some distance
     print(f"{name} matrix distance: {torch.linalg.norm(matrix_1 - matrix_2):.5f}")
