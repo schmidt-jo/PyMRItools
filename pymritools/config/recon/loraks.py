ae53f7ba66967a2664bcca2f3c959610406bdc1a
Jochen Schmidt <jschmidt@cbs.mpg.de>
2025-05-09 10:33:07 +0200

    fix typos and default settings

--- pymritools/config/recon/loraks.py
+++ pymritools/config/recon/loraks.py
@@ -18,22 +18,6 @@ class Settings(BaseClass):
         alias="-ia", default="",
         help="input affine matrix, necessary if input file is .pt to output .nii file."
     )
-    # in_sampling_mask: str = field(
-    #     alias="-is", default="",
-    #     help=f"(Optional) Input sampling mask for reconstruction masking sampled voxels in the input."
-    #          f" If not given it will be deduced from the input."
-    # )
-    # vars
-    # coil_compression: int = field(
-    #     alias="-cc", default=32,
-    #     help=f"Specify coil compression for multi channel data. Default working mode is "
-    #          f"Joint-Echo-Channel reconstruction, which can lead to memory problems. "
-    #          f"Compression can help in those cases."
-    # )
-    # read_dir: int = field(
-    #     alias="-rd", default=0,
-    #     help="specify read direction if not in x. Necessary for AC LORAKS to deduce AC region."
-    # )
     process_slice: bool = field(
         alias="-ps", default=False,
         help="toggle processing of middle slize and not whole volume, eg. for testing LORAKS parameters."
@@ -42,18 +26,11 @@ class Settings(BaseClass):
         alias="-r", default=3,
         help="Loraks neighborhood radius."
     )
-    # C matrix
-    # c_rank: int = field(alias="-cr", default=150, help="Rank for C matrix formalism.")
-    # c_lambda: float = field(
-    #     alias="-cl", default=0.1,
-    #     help=f"regularization parameter for Loraks C matrix "
-    #          f"rank minimization. Set 0.0 to disable C regularization."
-    # )
     rank: int = field(alias="-rank", default=250)
     reg_lambda: float = field(
-        alias="-l", default=0.1,
-        help=f"regularization parameter for Loraks S matrix "
-             f"rank minimization. Set 0.0 to disable S regularization."
+        alias="-l", default=0.0,
+        help=f"regularization parameter for Loraks matrix "
+             f"rank minimization. Set 0.0 to use true data consistency (default)."
     )
     matrix_type: str = choice(
         "S", "C", alias="-t", default="S",
