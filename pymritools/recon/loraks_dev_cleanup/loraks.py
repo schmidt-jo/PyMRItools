2dac9b76af8eb0bf333c8094b8f4d8a75b6c65dd
jochen.schmidt <jochen.schmidt@tuta.io>
2025-05-14 10:56:46 +0200

    add utility functions

--- pymritools/recon/loraks_dev_cleanup/loraks.py
+++ pymritools/recon/loraks_dev_cleanup/loraks.py
@@ -3,7 +3,7 @@ LORAKS algorithm for reconstruction of MRI data.
 
 The end user interface is set up here,
 We then have 2 LORAKS "flavors: AC-LORAKS and P-LORAKS.
-AC is an option for data where an autocalibration region is available and
+AC is an option for data where an auto-calibration region is available and
 can be used to speed up computations and reduce memory consumption
 Both are based on the Low rank modelling of k-space described originally by Haldar et al.
 https://pubmed.ncbi.nlm.nih.gov/24595341/;
@@ -32,14 +32,16 @@ The algorithm is using torch autograd to perform direct optimization of the Mini
 import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-
-import torch
-import tqdm
-
 from enum import Enum, auto
 from typing import Optional, final, TypeVar
 
 from simple_parsing.helpers import Serializable
+import torch
+import tqdm
+
+from pymritools.recon.loraks_dev_cleanup.utils import prepare_k_space_to_batches, unprepare_batches_to_k_space
+
+
 
 logger = logging.getLogger("Loraks")
 
@@ -90,6 +92,7 @@ class LoraksOptions(Serializable):
     rank: RankReduction = field(
         default_factory=lambda: RankReduction(method=RankReductionMethod.HARD_CUTOFF, value=150))
     regularization_lambda: float = 0.1
+    batch_size_channels: int = -1
     max_num_iter: int = 20
     device: torch.device = field(default_factory=lambda: torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
 
@@ -130,14 +133,6 @@ class LoraksBase(ABC):
         raise NotImplementedError("Subclasses must implement this method")
 
     @abstractmethod
-    def _initialize(self, k_space: torch.Tensor):
-        """
-        Used to initialize the reconstruction algorithm once before processing each batch.
-        This method can be implemented by subclasses and is called once before the reconstruction starts.
-        """
-        pass
-
-    @abstractmethod
     def _prepare_batch(self, batch):
         raise NotImplementedError("Subclasses must implement this method")
 
@@ -172,28 +167,9 @@ class LoraksBase(ABC):
         :raises ValueError: If the input tensor does not match the expected shape.
         :return: Output tensor divided into batches, reshaped to [b, -1].
         """
-        # Validate input shape
-        if k_space.dim() > 5:
-            raise ValueError("Input tensor must have 5 dimensions: [nt, ne, nz, ny, nx].")
-        while k_space.dim() < 5:
-            logger.warning(
-                f"Input tensor has less than 5 dimensions ({k_space.shape})."
-                f" We unfold last dimension, might create unwanted behaviour.")
-            k_space = k_space.unsqueeze(-1)
-        # TODO: we can include the padding here as well
-        input_shape = k_space.shape
+        # how to compute the channel batching?
 
-        # decide on matrix sizes, maybe from memory computations? first create matrices from channels and or contrasts.
-        # thus we have spatial dimensions and some combination of channels and contrasts -> [bce, nxyz, nce]
-        # ToDo: device combination strategy based on memory computations (those might differ per algorithm used),
-        #  should this be an abstract method again?
-        k_combined = NotImplemented
-        # we flatten everything per batch as we work with linear indices
-        b, nxyz, nce = k_combined.shape
-        combined_shape = (b, nxyz, nce)
-
-        k_batched = k_combined.reshape(b, -1)
-
+        k_batched, input_shape, combined_shape = prepare_k_space_to_batches(k_space=k_space, batch_size_channels=-1)
         return k_batched, input_shape, combined_shape
 
     @final
@@ -204,12 +180,7 @@ class LoraksBase(ABC):
         :param k_batches:
         :return:
         """
-        # unflatten the shape dim (need some shapes here...)
-        # need some information about channel batching (we might take this from above method as well?)
-        k_combined = k_batches.reshape(combined_shape)
-        # unfold individual dimensions - might need to process some channel / echo combinations
-        k_space = k_combined.reshape(input_shape)
-        # TODO: we need the unpadding here
+        k_space = unprepare_batches_to_k_space()
         return k_space
 
 
