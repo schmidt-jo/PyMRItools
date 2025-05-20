028fed4da8720361358cd230e937a47091a84598
jochen.schmidt <jochen.schmidt@tuta.io>
2025-05-20 13:58:00 +0200

    add recon script draft

---
+++ performance_experiments/loraks/reconstruction.py
@@ -0,0 +1,21 @@
+import logging
+
+from performance_experiments.utils import get_output_dir
+from pymritools.config.recon.loraks import Settings
+from pymritools.recon.loraks_dev_cleanup.recon import main as reco
+
+
+def main():
+    path_out = get_output_dir("loraks_recon")
+    config = Settings(
+        loraks_algorithm="AC-LORAKS",
+        out_path=path_out.as_posix(),
+        in_k_space="C:\\Daten_Jo\\Daten\\03_work\\06_owncloud\\data\\data\\dev\\loraks\\phantom_k_us_cs.pt",
+        max_num_iter=500
+    )
+    reco(config)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
