2cfbe1714a1970d991ef9227a215a033a57da94f
Patrick Scheibe <patrick@halirutan.de>
2025-06-23 14:38:41 +0200

    Refactor utility functions: improve code readability, add detailed docstrings, and fix minor typos.

--- pymritools/utils/functions.py
+++ pymritools/utils/functions.py
@@ -1,12 +1,10 @@
 import numpy as np
 import torch
-import logging
-log_module = logging.getLogger(__name__)
 
 
 def interpolate(x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor) -> torch.Tensor:
     """
-    Interpolate a function fp at points xp in a multidimensional context
+    Interpolates a function fp at points xp in a multidimensional context
 
     Parameters:
     x (torch.Tensor): Tensor of the new sampling points with shape [batch, a, b]
@@ -20,13 +18,13 @@ def interpolate(x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor) -> torch.Te
         # in case there is no batch dim we fill
         x = x.unsqueeze(0)
     batch, a, b = x.shape
-    # find closest upper adjacent indices of x in xp, then the next lower one
+    # find the closest upper adjacent indices of x in xp, then the next lower one
     indices = torch.searchsorted(xp, x.view(-1, b))
     indices = torch.clamp(indices, 1, xp.shape[0] - 1)
     # find adjacent left and right points on originally sampled axes xp
     x0 = xp[indices - 1]
     x1 = xp[indices]
-    # find values of originally sampled function considering its differing for each idx_a
+    # find values of the originally sampled function considering its differing for each idx_a
     fp_expanded = fp.unsqueeze(0).expand(batch, -1, -1)
     y0 = fp_expanded.gather(2, indices.view(batch, a, b) - 1)
     y1 = fp_expanded.gather(2, indices.view(batch, a, b))
@@ -37,8 +35,20 @@ def interpolate(x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor) -> torch.Te
 
 
 def normalize_data(data: torch.Tensor, dim_t: int = -1) -> (torch.Tensor, torch.Tensor):
+    """
+    Normalizes input tensor along a specified dimension using its L2-norm and handles cases
+    with NaN or infinity values.
+
+    Args:
+        data: Input tensor to be normalized.
+        dim_t: Dimension along which to calculate the norm. Defaults to -1 (the last dimension).
+
+    Returns:
+        A tuple containing:
+            - The normalized tensor, where the L2-norm divides each element along the specified dimension.
+            - The L2-norm tensor along the specified dimension.
+    """
     norm_factor = torch.linalg.norm(data, dim=dim_t, keepdim=True)
-    # normalize
     norm_data = torch.nan_to_num(
         torch.divide(data, norm_factor),
         nan=0.0, posinf=0.0, neginf=0.0
@@ -48,9 +58,24 @@ def normalize_data(data: torch.Tensor, dim_t: int = -1) -> (torch.Tensor, torch.
 
 def fft_to_img(
         input_data: np.ndarray | torch.Tensor,
-        dims: tuple | int = (-1, -2)):
+        dims: tuple | int = (-1, -2)) -> (np.ndarray | torch.Tensor):
+    """
+    Performs an N-dimensional Fourier transform on the input tensor or array, with appropriate 
+    shifting to move the zero-frequency component to the center. This is useful for converting 
+    frequency domain data to an image representation or for processing data that has been stored 
+    in the Fourier domain.
+
+    Args:
+        input_data: The input data to be transformed.
+        dims: The dimensions over which the FFT operation should be applied.
+            Defaults to (-1, -2).
+
+    Returns:
+        np.ndarray | torch.Tensor: The transformed data with zero-frequency components shifted 
+        to the center. The output format matches the type of the input (`np.ndarray` if input 
+        is a NumPy array, `torch.Tensor` if input is a PyTorch tensor).
+    """
     if isinstance(dims, int):
-        # make tuple
         dims = (dims,)
     if torch.is_tensor(input_data):
         return torch.fft.fftshift(
@@ -78,9 +103,20 @@ def fft_to_img(
 
 def ifft_to_k(
         input_data: np.ndarray | torch.Tensor,
-        dims: tuple | int = (-1, -2)):
+        dims: tuple | int = (-1, -2)) -> (np.ndarray | torch.Tensor):
+    """
+    Performs an inverse FFT on the input data and shifts the zero-frequency component
+    to the center of the spectrum for k-space conversion.
+
+    Args:
+        input_data: The input array or tensor containing data for which the inverse FFT needs to be computed.
+        dims: A tuple or integer specifying the axes along which the inverse FFT is computed. Defaults to (-1, -2).
+
+    Returns:
+        The transformed input data after applying the inverse FFT and necessary shifts. The result has the same type
+        as the input_data.
+    """
     if isinstance(dims, int):
-        # make tuple
         dims = (dims,)
     if torch.is_tensor(input_data):
         return torch.fft.fftshift(
@@ -106,7 +142,17 @@ def ifft_to_k(
         )
 
 
-def root_sum_of_squares(input_data: np.ndarray | torch.Tensor, dim_channel: int = -1):
+def root_sum_of_squares(input_data: np.ndarray | torch.Tensor, dim_channel: int = -1) -> (np.ndarray | torch.Tensor):
+    """
+    Computes the root sum of squares (RSS) along a specified channel dimension for the input data.
+
+    Args:
+        input_data: The input data to compute the RSS for.
+        dim_channel (int, optional): The dimension along which to compute the RSS. Defaults to -1.
+
+    Returns:
+        The computed root sum of squares. The output type is the same as the input type.
+    """
     if torch.is_tensor(input_data):
         return torch.sqrt(
             torch.sum(
@@ -123,45 +169,68 @@ def root_sum_of_squares(input_data: np.ndarray | torch.Tensor, dim_channel: int 
         )
 
 
-def gaussian_window(size: int, sigma: float, center: int = None):
+def gaussian_window(size: int, sigma: float, center: int = None) -> torch.Tensor:
+    """
+    Creates a 1-dimensional Gaussian window.
+
+    The function generates a Gaussian window of a given size with a specified
+    standard deviation (sigma). Optionally, the user can provide the center
+    of the Gaussian; if not provided, the center is calculated as the middle
+    of the window.
+
+    Args:
+        size: The size of the Gaussian window.
+        sigma: The standard deviation of the Gaussian.
+        center: The center point of the Gaussian. If not specified, the center is set to the middle of the window based
+            on the size.
+
+    Returns:
+        torch.Tensor: A 1-dimensional tensor representing the Gaussian window.
+
+    Raises:
+        None
+    """
     if center is None:
         center = int(np.round(size / 2))
     ax = torch.arange(size).float()
     return torch.exp(-((ax - center) ** 2) / (2 * sigma ** 2))
 
 
-
-def gaussian_2d_kernel(size_x: int, size_y: int, center_x: int=None, center_y:int=None,
+def gaussian_2d_kernel(size_x: int, size_y: int, center_x: int = None, center_y: int = None,
                        sigma: float | tuple[float, float] = 1.0):
     """
     Generates a 2D Gaussian kernel.
 
-    Parameters:
-    - size_x (int): Kernel size in the x dimension.
-    - size_y (int): Kernel size in the y dimension.
-    - center_x (float): Center of the Gaussian in the x dimension. If None, defaults to the center of the kernel.
-    - center_y (float): Center of the Gaussian in the y dimension. If None, defaults to the center of the kernel.
-    - sigma (float | tuple of floats): Standard deviation of the Gaussian.
-        If a tuple is given the function uses different gaussian scale per dim respectively.
+    Args:
+        size_x: Kernel size in the x dimension.
+        size_y: Kernel size in the y dimension.
+        center_x: Center of the Gaussian in the x dimension. If None, defaults to the center of the kernel.
+        center_y: Center of the Gaussian in the y dimension. If None, defaults to the center of the kernel.
+        sigma: Standard deviation of the Gaussian. If a tuple is provided, the function uses different Gaussian
+            scale per dim respectively.
 
     Returns:
-    - kernel (torch.Tensor): 2D Gaussian kernel.
+        2D Gaussian kernel
     """
     if isinstance(sigma, float | int):
         sigma = (sigma, sigma)
     gauss_x = gaussian_window(size_x, sigma[0], center=center_x)
-    guass_y = gaussian_window(size_y, sigma[1], center=center_y)
+    gauss_y = gaussian_window(size_y, sigma[1], center=center_y)
 
     gauss_2d = torch.matmul(
-        gauss_x.unsqueeze(-1), guass_y.unsqueeze(0)
+        gauss_x.unsqueeze(-1), gauss_y.unsqueeze(0)
     )
-    # normalize
     gauss_2d /= gauss_2d.sum()
-
     return gauss_2d
 
 
 class SimpleKalmanFilter:
+    """A simple implementation of a Kalman filter for estimating a value.
+
+    The Kalman filter is used to provide a more accurate estimate of a value by combining predictions from a model and
+    noisy observations, commonly used for filtering noise in measurements or tracking signals.
+    """
+
     def __init__(self, process_variance=1e-5, measurement_variance=1e-3):
         self.process_variance = process_variance
         self.measurement_variance = measurement_variance
@@ -169,6 +238,18 @@ class SimpleKalmanFilter:
         self.error_estimate = 1
 
     def update(self, measurement):
+        """
+        Updates the estimate of the system state based on the new measurement using the Kalman Filter algorithm.
+        It calculates the Kalman Gain, updates the current estimate based on the measurement,
+        and adjusts the error estimate accordingly.
+
+        Args:
+            measurement: The new measurement data to incorporate into the current
+                state estimate.
+
+        Returns:
+            The updated state estimate after incorporating the new measurement.
+        """
         if self.estimate is None:
             self.estimate = measurement
             return self.estimate
@@ -186,10 +267,21 @@ class SimpleKalmanFilter:
         return self.estimate
 
 
-def exponential_moving_average(new_value, previous_smoothed, alpha=0.1):
+def exponential_moving_average(new_value: float, previous_smoothed: float, alpha: float = 0.1) -> float:
     """
-    Exponential Moving Average smoothing
-    - alpha controls the smoothing (0 < alpha < 1)
-    - Lower alpha = more smoothing, higher alpha = less smoothing
+    Calculates the exponential moving average of a time series data point.
+
+    This function computes the exponentially weighted moving average given a new data point, the previous smoothed
+    value, and a smoothing factor (alpha). It is often used in signal processing and time series analysis to produce
+    a smoothed dataset.
+
+    Args:
+        new_value: Current data point in the time series.
+        previous_smoothed: Previously calculated smoothed value.
+        alpha: Smoothing factor, a value between 0 and 1, which determines the weight given to new data relative to
+            the previous smoothed value. Defaults to 0.1.
+
+    Returns:
+        The updated smoothed value calculated as the exponential moving average.
     """
     return alpha * new_value + (1 - alpha) * previous_smoothed
